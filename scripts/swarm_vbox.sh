0b7648d3a8c7be017fbddb8a6ad113e6bf678f30
sthordall <stephantl@gmail.com>
2016-11-27 15:50:25 +0100

    Docker swarm script almost there

--- scripts/swarm_vbox.sh
+++ scripts/swarm_vbox.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-MANAGER=3
-WORKER=5
+MANAGER=2
+NODE=2
 
-echo "Creating Docker hosts, $MANAGER managers and $WORKER workers..."
+echo "Creating initial Docker host..."
+docker-machine create -d virtualbox local >> /dev/null
+eval "$(docker-machine env local)"
+echo "Creating swarm on initial Docker host..."
+SWARM_CLUSTER_TOKEN=$(docker run swarm create)
+echo
+
+echo "Creating Docker hosts, $MANAGER managers and $NODE nodes..."
 for i in $(seq 1 $MANAGER)
 do 
 	echo "	-> creating manager$i"
-	docker-machine create --driver virtualbox manager$i >> /dev/null
+	docker-machine create \
+    -d virtualbox \
+    --swarm \
+    --swarm-master \
+    --swarm-discovery token://$SWARM_CLUSTER_TOKEN \
+    manager$i >> /dev/null
 done
-for i in $(seq 1 $WORKER)
+for i in $(seq 1 $NODE)
 do
-	echo "	-> creating worker$i"
-	docker-machine create --driver virtualbox worker$i >> /dev/null
+	echo "	-> creating node$i"
+	docker-machine create \
+    -d virtualbox \
+    --swarm \
+    --swarm-discovery token://$SWARM_CLUSTER_TOKEN \
+    node$i >> /dev/null
 done
 echo
-
-echo "Initializing swarm on manager1..."
-docker-machine ssh manager1 docker swarm init --auto-accept manager --auto-accept worker --listen-addr $(docker-machine ip manager1):2377 >> /dev/null
-echo
-
-echo "Adding additional managers to swarm..."
-for i in $(seq 2 $MANAGER)
-do 
-	echo "	-> manager$i joining"
-	docker-machine ssh manager$i docker swarm join --manager --listen-addr $(docker-machine ip manager$i):2377 $(docker-machine ip manager1):2377
-done
-
-for i in $(seq 1 $WORKER)
-do
-	echo "	-> worker$i joining"
-	docker-machine ssh worker$i docker swarm join --listen-addr $(docker-machine ip worker$i):2377 $(docker-machine ip manager1):2377
-done
 
 echo "Done!"
+echo 'Run following to connect to Swarm: eval "$(docker-machine env --swarm manager1)"'
