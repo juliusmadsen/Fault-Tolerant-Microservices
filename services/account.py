25f9348ec989268769b980bba043aa98bbd96df8
Julius Taul Madsen <julius.madsen@gmail.com>
2016-11-27 13:20:36 +0100

    Multithreaded to avoid PUT hanging

--- services/account.py
+++ services/account.py
@@ -7,28 +7,22 @@ import redis
 app = Flask(__name__)
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-"""
+
 @app.route("/account/<accountId>", methods=['PUT'])
-def account_info(accountId):
+def account_put(accountId):
+    amount = request.get_json()['amount']
+    newAmount = r.hincrby("user:" + accountId, "amount", amount)
+    return nice_json({
+        "amount": newAmount
+    })
+
+@app.route("/account/<accountId>", methods=['GET'])
+def account_get(accountId):
     amount = r.hget("user:" + accountId, "amount")
     return nice_json({
         "amount": amount
     })
-"""
-@app.route("/account/<accountId>", methods=['GET', 'PUT'])
-def account_info(accountId):
-    if request.method == 'PUT':
-        amount = request.get_json()['amount']
-        newAmount = r.hincrby("user:" + accountId, "amount", amount)
-        return nice_json({
-            "amount": newAmount
-        })
-    elif request.method == 'GET':
-        amount = r.hget("user:" + accountId, "amount")
-        return nice_json({
-            "amount": amount
-        })
-
+    
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+    app.run(port=5001, debug=True, threaded=True)
 
