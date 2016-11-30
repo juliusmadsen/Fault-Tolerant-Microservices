6b0183ec5b86c121c341e262ed35a73bd025a62c
sthordall <stephantl@gmail.com>
2016-11-30 16:54:51 +0100

    MUCH BENCHMARK SCRIPT

--- services/trader.py
+++ services/trader.py
@@ -8,15 +8,16 @@ import threading
 
 app = Flask(__name__)
 
-request_timeout = 0.08
-
-max_connections = 3
+request_timeout = 0.11
+max_connections = 100
+circuit_maxfail = 3
+circuit_timeout = 1
 
 account_sema = threading.BoundedSemaphore(value=max_connections)
-account_circuit = CircuitBreaker("account", 3, 1)
+account_circuit = CircuitBreaker("account", circuit_maxfail, circuit_timeout)
 
 stock_sema = threading.BoundedSemaphore(value=max_connections)
-stock_circuit = CircuitBreaker("stock", 3, 1)
+stock_circuit = CircuitBreaker("stock", circuit_maxfail, circuit_timeout)
 
 def getAccount(accountId):
     global account_sema
@@ -25,7 +26,7 @@ def getAccount(accountId):
     
     account_sema.acquire()
     res = account_circuit.call(lambda:
-                               requests.get("http://localhost:5001/account/" + str(accountId),
+                               requests.get("http://account:5001/account/" + str(accountId),
                                             timeout=request_timeout))
     account_sema.release()
     return res.json()
@@ -37,7 +38,7 @@ def getStock(stockName):
     
     stock_sema.acquire()
     res = stock_circuit.call(lambda:
-                             requests.get("http://localhost:5002/stock/" + str(stockName),
+                             requests.get("http://stocks:5002/stock/" + str(stockName),
                                           timeout=request_timeout))
     stock_sema.release()
     return res.json()
@@ -50,7 +51,7 @@ def updateAccount(accountId, amount, stockName, stockAmount):
     payload = { "amount": amount, "stock": {"name": stockName, "amount": stockAmount} }
     account_sema.acquire()
     res = account_circuit.call(lambda:
-                               requests.put("http://localhost:5001/account/" + str(accountId),
+                               requests.put("http://account:5001/account/" + str(accountId),
                                             timeout=request_timeout,
                                             json=payload))
     account_sema.release()
@@ -101,6 +102,55 @@ def stock_update():
     
     return nice_json(res)
 
+@app.route("/setup", methods=['GET'])
+def setup_get():
+    global request_timeout
+    global max_connections
+    global circuit_maxfail
+    global circuit_timeout
+
+    res = { "request_timeout": request_timeout,
+            "max_connections": max_connections,
+            "circuit_maxfail": circuit_maxfail,
+            "circuit_timeout": circuit_timeout }
+
+    return nice_json(res)
+
+@app.route("/setup", methods=['PUT'])
+def setup_update():
+    try:
+        req = request.get_json(force=True)
+
+        global request_timeout
+        global max_connections
+        global circuit_maxfail
+        global circuit_timeout
+
+        request_timeout = req["request_timeout"]
+        max_connections = req["max_connections"]
+        circuit_maxfail = req["circuit_maxfail"]
+        circuit_timeout = req["circuit_timeout"]
+
+        global account_sema
+        global account_circuit
+        global stock_sema
+        global stock_circuit
+
+        account_sema = threading.BoundedSemaphore(value=max_connections)
+        account_circuit = CircuitBreaker("account", circuit_maxfail, circuit_timeout)
+
+        stock_sema = threading.BoundedSemaphore(value=max_connections)
+        stock_circuit = CircuitBreaker("stock", circuit_maxfail, circuit_timeout)
+
+        res = { "request_timeout": request_timeout,
+                "max_connections": max_connections,
+                "circuit_maxfail": circuit_maxfail,
+                "circuit_timeout": circuit_timeout }
+    except:
+        return error_response("Setup update failed")
+
+    return nice_json(res)
+
 if __name__ == "__main__":
-    app.run(port=5003, host="localhost", debug=True, threaded=True)
+    app.run(port=5003, host="0.0.0.0", debug=True, threaded=True)
 
--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -1,2 +1,48 @@
 #!/bin/bash
-ab -n 10000 -c 100 -T application/json -u data.json http://127.0.0.1:5003/
+SERVICE_HOST=$(docker-machine ip manager-1)
+RESULT_FILE=results
+
+function putJson {
+	URL=$1
+	JSON=$2
+	curl -H 'Content-Type: application/json' \
+			 -H 'Accept: application/json' \
+			 -X PUT \
+			 -d "$JSON" \
+			 $URL
+}
+
+echo "Putting test data to account service..."
+putJson $SERVICE_HOST/account/1 '{"amount": 99999999999 }'
+echo
+
+echo "Putting test data to account service..."
+putJson $SERVICE_HOST/stock/novo '{"price": 1 }'
+echo
+
+ftsetups=(
+	'{ "request_timeout": 0.5, "max_connections": 100, "circuit_maxfail": 5, "circuit_timeout": 1 }'
+	'{ "request_timeout": 0.6, "max_connections": 100, "circuit_maxfail": 5, "circuit_timeout": 1 }'
+	'{ "request_timeout": 0.7, "max_connections": 100, "circuit_maxfail": 5, "circuit_timeout": 1 }'
+	'{ "request_timeout": 0.8, "max_connections": 100, "circuit_maxfail": 5, "circuit_timeout": 1 }'
+	'{ "request_timeout": 0.9, "max_connections": 100, "circuit_maxfail": 5, "circuit_timeout": 1 }'
+)
+
+echo > $RESULT_FILE
+for setup in "${ftsetups[@]}"
+do
+	echo "Load new fault tolerant setup..."
+	echo "$setup" >> $RESULT_FILE
+	putJson $SERVICE_HOST/trade/setup "$setup"
+	echo
+	
+	echo "Benchmarking..."
+	ab -n 100 -c 10 -T application/json -u data.json $SERVICE_HOST/trade/ \
+	| grep -E 'Complete|Failed|Time taken|Time per' >> $RESULT_FILE
+	echo "Benchmarking done."
+	echo
+	echo >> $RESULT_FILE
+done
+
+echo "Results stored in file: $RESULT_FILE"
+
